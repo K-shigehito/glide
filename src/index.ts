52d41d985babef2530a215e0dcb1a80e735dea81
K-shigehito <example@test.com>
2023-02-02 17:30:47 +0900

    fix: config

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import { readConfig } from './lib/readConfig';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 
+const appConfig = readConfig();
+
 yargs(hideBin(process.argv))
   .commandDir('commands')
-  .config(readConfig())
+  .config(appConfig)
   .strict()
   .alias({ h: 'help', v: 'version' }).argv;
